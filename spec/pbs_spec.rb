83458aff75278c27fcdf3ac58714d6e185b327df
Jeremy Nicklas <jnicklas@osc.edu>
2014-11-18 10:46:42 -0500

    updated tests

--- spec/pbs_spec.rb
+++ spec/pbs_spec.rb
@@ -1,21 +1,28 @@
-describe PBS do
-  include PBS
+RSpec.describe PBS do
+
+  subject { PBS }
 
   # methods
-  it { should respond_to(:pbs_connect) }
-  it { should respond_to(:pbs_default) }
-  it { should respond_to(:pbs_deljob) }
-  it { should respond_to(:pbs_disconnect) }
-  it { should respond_to(:pbs_statfree) }
-  it { should respond_to(:pbs_statjob) }
-  it { should respond_to(:pbs_statnode) }
-  it { should respond_to(:pbs_statque) }
-  it { should respond_to(:pbs_statserver) }
-  it { should respond_to(:pbs_submit) }
-  it { should respond_to(:error) }
-  it { should respond_to(:error?) }
+  it { is_expected.to respond_to(:pbs_connect) }
+  it { is_expected.to respond_to(:pbs_default) }
+  it { is_expected.to respond_to(:pbs_deljob) }
+  it { is_expected.to respond_to(:pbs_disconnect) }
+  it { is_expected.to respond_to(:pbs_statfree) }
+  it { is_expected.to respond_to(:pbs_statjob) }
+  it { is_expected.to respond_to(:pbs_statnode) }
+  it { is_expected.to respond_to(:pbs_statque) }
+  it { is_expected.to respond_to(:pbs_statserver) }
+  it { is_expected.to respond_to(:pbs_submit) }
+  it { is_expected.to respond_to(:error) }
+  it { is_expected.to respond_to(:error?) }
 
-  describe "a local PBS server" do
-    
+  describe "::pbs_default" do
+    subject { PBS::pbs_default() }
+
+    let(:pbs_server) { `qstat -q | awk 'FNR == 2 {print $2}'`.chomp! }
+
+    context 'when local PBS server is found' do
+      it { is_expected.to eq(pbs_server) }
+    end
   end
 end
