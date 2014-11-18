cbacf834c72e8228b3f36cd9947d69ed163b456d
Jeremy Nicklas <jnicklas@osc.edu>
2014-11-18 16:27:59 -0500

    added connection tests

--- spec/pbs_spec.rb
+++ spec/pbs_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe PBS do
   it { is_expected.to respond_to(:pbs_submit) }
   it { is_expected.to respond_to(:error) }
   it { is_expected.to respond_to(:error?) }
+  it { is_expected.to respond_to(:reset_error) }
 
   describe "::pbs_default" do
     subject { PBS::pbs_default() }
@@ -23,6 +24,29 @@ RSpec.describe PBS do
 
     context 'when local PBS server is found' do
       it { is_expected.to eq(pbs_server) }
+    end
+  end
+
+  describe "::pbs_connect" do
+    subject(:conn) { PBS::pbs_connect(server) }
+
+    context "when connecting to local server" do
+      let(:server) { PBS::pbs_default() }
+      it "provides connection number" do
+        expect(conn).to be > 0
+      end
+      it "doesn't raise an error" do
+        expect { conn }.to_not raise_error
+      end
+      after { PBS::pbs_disconnect(conn) }
+    end
+
+    context "when connecting to bad server" do
+      let(:server) { 'bad.server' }
+      it "raises a PBS error" do
+        expect { conn }.to raise_error PBS::PBSError, 'access from host not allowed'
+      end
+      after { PBS::reset_error }
     end
   end
 end
