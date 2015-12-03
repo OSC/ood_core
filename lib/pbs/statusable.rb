3a40e434021b0f4fe478963ef9f16e5466ccdddb
Jeremy Nicklas <jnicklas@osc.edu>
2015-12-03 11:56:36 -0500

    small refactor for statusable

--- lib/pbs/statusable.rb
+++ lib/pbs/statusable.rb
@@ -2,9 +2,8 @@ module PBS
   module Statusable
     # Get status of job by creating a Query object
     def status(args = {})
-      args.merge!({id: id})
       q = Query.new(type: :job, conn: conn)
-      q.find(args)[0]
+      q.find(args.merge(id: id))[0]
     end
   end
 end
