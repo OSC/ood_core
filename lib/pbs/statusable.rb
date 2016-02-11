304a716e6785f141bb8beb28b1974e5ef481faf3
Jeremy Nicklas <jnicklas@osc.edu>
2016-02-11 15:42:32 -0500

    move all job dependencies to job file
    fixes #6

--- lib/pbs/statusable.rb
+++
@@ -1,9 +0,0 @@
-module PBS
-  module Statusable
-    # Get status of job by creating a Query object
-    def status(args = {})
-      q = Query.new(type: :job, conn: conn)
-      q.find(args.merge(id: id))[0]
-    end
-  end
-end
