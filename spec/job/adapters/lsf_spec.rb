d35a6e0df24a0b4e912c804806819a1f16730bfb
Eric Franz <efranz@osc.edu>
2017-04-01 00:29:44 -0400

    clean up tests

--- spec/job/adapters/lsf_spec.rb
+++ spec/job/adapters/lsf_spec.rb
@@ -2,11 +2,7 @@ require "spec_helper"
 require "ood_core/job/adapters/lsf"
 
 describe OodCore::Job::Adapters::Lsf do
-  # Required arguments
-  let(:config) { {} }
-
-  # Subject
-  subject(:adapter) { described_class.new(config) }
+  subject(:adapter) { described_class.new({}) }
 
   it { is_expected.to respond_to(:submit).with(0).arguments.and_keywords(:script, :after, :afterok, :afternotok, :afterany) }
   it { is_expected.to respond_to(:info).with(0).arguments.and_keywords(:id) }
@@ -28,15 +24,12 @@ describe OodCore::Job::Adapters::Lsf do
   # parse_bjobs_output
   describe "Batch#parse_bjobs_output" do
     subject(:batch) { OodCore::Job::Adapters::Lsf::Batch.new() }
-    let(:job_hash) {
-    }
 
-
-    it "should handle no jobs in output" do
+    it "handles no jobs in output" do
       expect(batch.parse_bjobs_output "No job found\n").to eq [{}]
     end
 
-    it "should raise exception for unexpected columns" do
+    it "raises exception for unexpected columns" do
       # I added ANOTHER_COLUMN to the end of this
       output = <<-OUTPUT
 JOBID   USER    STAT  QUEUE
@@ -45,7 +38,7 @@ JOBID   USER    STAT  QUEUE
       expect { batch.parse_bjobs_output output }.to raise_error(OodCore::Job::Adapters::Lsf::Batch::Error)
     end
 
-    it "should parse output for one job" do
+    it "parses output for one job" do
       output = <<-OUTPUT
 JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME  PROJ_NAME CPU_USED MEM SWAP PIDS START_TIME FINISH_TIME
 542935  efranz  RUN   short      foobar02.osc.edu compute013  foo        03/31-14:46:42 default    000:00:00.00 2      32     25156 03/31-14:46:44 -
@@ -69,7 +62,7 @@ JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME 
        }])
     end
 
-    it "should parse output for two jobs" do
+    it "parses output for two jobs" do
       output = <<-OUTPUT
 JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME  PROJ_NAME CPU_USED MEM SWAP PIDS START_TIME FINISH_TIME
 542935  efranz  RUN   short      foobar02.osc.edu compute013  foo        03/31-14:46:42 default    000:00:00.00 2      32     25156 03/31-14:46:44 -
@@ -111,7 +104,7 @@ JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME 
        }])
     end
 
-    it "should parse output for piped script with no jobname" do
+    it "parses output for piped script with no jobname" do
       output = <<-OUTPUT
 JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME  PROJ_NAME CPU_USED MEM SWAP PIDS START_TIME FINISH_TIME
 542945  efranz  DONE  short      foobar02.osc.edu compute013  #!/bin/bash;#;#BSUB -q short # queue;#BSUB -e myjob.%J.err;#BSUB -o myjob.%J.out; echo "Hello world, I am in $PWD";sleep 30;echo "Goodbye, world!" 03/31-19:24:57 default    000:00:00.03 2      32     9389 03/31-19:24:59 03/31-19:25:29
