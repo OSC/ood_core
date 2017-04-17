19f713d98bf7deb631b020dbd254f03f6028d2a7
Eric Franz <efranz@osc.edu>
2017-04-17 14:58:20 -0400

    move Lsf date parsing method to helper class

--- lib/ood_core/job/adapters/lsf/batch.rb
+++ lib/ood_core/job/adapters/lsf/batch.rb
@@ -124,27 +124,6 @@ class OodCore::Job::Adapters::Lsf::Batch
     end
   end
 
-  # convert string in format "03/31-14:46:42" to Time object
-  # assumes time being parsed is a time that ocurred in the past
-  # not to be used for parsing times in the future (like estimated FINISH_TIME)
-  def parse_past_time(t, ignore_errors: false)
-    return nil if t.nil? || t.empty? || t == "-"
-    year = Time.now.year
-    time = Time.parse("#{year}/#{t}")
-
-    # handle edge case where job started before new year
-    time = Time.parse("#{year - 1}/#{t}") if time.month > Time.now.month
-
-    time
-
-  rescue ArgumentError => e
-    raise e unless ignore_errors
-
-    #TODO: warn via logger
-
-    nil
-  end
-
   private
     # Call a forked Lsf command for a given cluster
     def call(cmd, *args, env: {}, stdin: "")
--- spec/job/adapters/lsf/batch_spec.rb
+++ spec/job/adapters/lsf/batch_spec.rb
@@ -167,48 +167,4 @@ OUTPUT
       )}
     end
   end
-
-  describe '#parse_past_time' do
-    it "converts time using current year" do
-      Timecop.freeze(Time.local(2017, 07, 01))
-
-      year = Time.now.year
-      expect(batch.parse_past_time("06/31-14:46:42")).to eq(Time.local(year, 6, 31, 14, 46, 42))
-      expect(batch.parse_past_time("05/31-12:46:42")).to eq(Time.local(year, 5, 31, 12, 46, 42))
-
-      Timecop.return
-    end
-
-    it "handles times from previous year" do
-      now = Time.local(2017, 01, 10)
-      Timecop.freeze(now)
-      expect(Time.now).to eq(now)
-
-      expect(batch.parse_past_time("12/27-12:22:22")).to eq(Time.local(2016, 12, 27, 12, 22, 22))
-
-      Timecop.return
-    end
-
-    context "with nil" do
-      it { expect(batch.parse_past_time(nil)).to eq(nil) }
-    end
-
-    context "with '-'" do
-      it { expect(batch.parse_past_time("-")).to eq(nil) }
-    end
-
-    context "with ''" do
-      it { expect(batch.parse_past_time("")).to eq(nil) }
-    end
-
-    context "with unparsable string raise ArgumentError" do
-      it "raises ArgumentError" do
-        expect { batch.parse_past_time("something not parsable") }.to raise_error(ArgumentError)
-      end
-
-      it "returns nil if ignore_errors:true" do
-        expect(batch.parse_past_time("something not parsable", ignore_errors: true)).to eq(nil)
-      end
-    end
-  end
 end
