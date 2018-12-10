8d5793992671a04c39ae64a35995384549bb287b
Morgan Rodgers <mrodgers@osc.edu>
2018-12-10 16:49:28 -0500

    Improve command override logic and rename std_bin to bin_default

--- lib/ood_core/job/adapters/helper.rb
+++ lib/ood_core/job/adapters/helper.rb
@@ -5,12 +5,12 @@ module OodCore
       class Helper
         # Get the configured path to a command allowing overrides from bin_overrides
         # @param cmd [String] the desired command
-        # @param std_bin [String] the default place to find that command
+        # @param bin_default [String] the default place to find cmd on the file system
         # @param bin_overrides [Hash<String, String>] commands associated with the full path to their replacement
         #   e.g. {'squeue' => '/usr/local/slurm/bin/squeue'}
         # @return [String] path to the configured command
-        def self.bin_path(cmd, std_bin, bin_overrides)
-          bin_overrides.fetch(cmd.to_s) { std_bin.join(cmd.to_s).to_s }
+        def self.bin_path(cmd, bin_default, bin_overrides)
+          bin_overrides.fetch(cmd.to_s) { Pathname.new(bin_default.to_s).join(cmd.to_s).to_s }
         end
       end
     end
