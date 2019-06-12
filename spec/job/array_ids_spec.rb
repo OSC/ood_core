a02e8fe9b33ec18f9a6a4a197168db61ee3b204c
Morgan Rodgers <mrodgers@osc.edu>
2019-06-12 09:38:18 -0400

    Add another test for an invalid array spec which the regex implementation fails

--- spec/job/array_ids_spec.rb
+++ spec/job/array_ids_spec.rb
@@ -47,11 +47,11 @@ describe OodCore::Job::ArrayIds do
         described_class.new('1,3-6:3,7,9-11,13,15-17,20-30:2').ids
       ).to eql([1, 3, 6, 7, 9, 10, 11, 13, 15, 16, 17, 20, 22, 24, 26, 28, 30])
     end
+  end
 
-    it "returns the correct IDs" do
-      expect( 
-        described_class.new('1,3-6:3,7,9-11,13,15-17,20-30:2%5').ids
-      ).to eql([1, 3, 6, 7, 9, 10, 11, 13, 15, 16, 17, 20, 22, 24, 26, 28, 30])
+  context "when the spec contains a percent modifier, but no number" do
+    it "returns an empty array" do
+      expect( described_class.new('1-4%').ids ).to eql([])
     end
   end
 
@@ -82,12 +82,6 @@ describe OodCore::Job::ArrayIds do
   context "when the spec is garbage" do
     it "returns an empty array" do
       expect(described_class.new('1!@-#$%^&*()DHKI%^&*(@)#@_!-=').ids).to eq([])
-    end
-  end
-
-  context "when the spec is invalid and ends with a comma" do
-    it "returns an empty array" do
-      expect(described_class.new('1-4%2,').ids).to eq([])
     end
   end
 end
