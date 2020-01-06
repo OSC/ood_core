b9694b8c1e67ffd524874c705352f333f67e8139
Morgan Rodgers <mrodgers@osc.edu>
2020-01-06 13:12:44 -0500

    Remove whitespace

--- lib/ood_core/job/adapters/sge/batch.rb
+++ lib/ood_core/job/adapters/sge/batch.rb
@@ -32,11 +32,11 @@ class OodCore::Job::Adapters::Sge::Batch
   # @api private
   # @see Factory.build_sge
   def initialize(config)
-    @cluster           = config.fetch(:cluster, nil)
-    @conf              = Pathname.new(config.fetch(:conf, nil))
-    @bin               = Pathname.new(config.fetch(:bin, nil))
-    @sge_root          = Pathname.new(config[:sge_root] || ENV['SGE_ROOT'] || "/var/lib/gridengine")
-    @bin_overrides     = config.fetch(:bin_overrides, {})
+    @cluster          = config.fetch(:cluster, nil)
+    @conf             = Pathname.new(config.fetch(:conf, nil))
+    @bin              = Pathname.new(config.fetch(:bin, nil))
+    @sge_root         = Pathname.new(config[:sge_root] || ENV['SGE_ROOT'] || "/var/lib/gridengine")
+    @bin_overrides    = config.fetch(:bin_overrides, {})
 
     # FIXME: hack as this affects env of the process!
     ENV['SGE_ROOT'] = @sge_root.to_s
