15007a225f87c39267b61f1e4afa23de2b1483ae
Eric Franz <efranz@osc.edu>
2021-04-23 09:28:06 -0400

    memoize Cluster#allow? #login_allow? & job_allow?

--- spec/cluster_spec.rb
+++ spec/cluster_spec.rb
@@ -2,28 +2,54 @@ require "spec_helper"
 
 describe OodCore::Cluster do
   describe "#==" do
-    context "when compared with another cluster" do
+    context "with two clusters that only specify ids" do
       let(:owens) { OodCore::Cluster.new({ :id => "owens" }) }
       let(:oakley) { OodCore::Cluster.new({ :id => "oakley" }) }
 
-      it "is equal" do
+      it "is not equal when comparing both clusters" do
         expect(owens).not_to eq(oakley)
       end
-    end
 
-    context "when compared with itself" do
-      let(:owens) { OodCore::Cluster.new({ :id => "owens" }) }
+      it "is not equal when comparing to nil" do
+        expect(owens).not_to eq(nil)
+      end
 
-      it "is equal" do
+      it "is equal when comparing single cluster with self" do
         expect(owens).to eq(owens)
       end
-    end
 
-    context "when compared with nil" do
-      let(:owens) { OodCore::Cluster.new({ :id => "owens" }) }
+      it "is allowed since there are no restrictions" do
+        expect(owens.allow?).to be true
+      end
 
-      it "is not equal" do
-        expect(owens).not_to eq(nil)
+      it "does not support login" do
+        expect(owens.login_allow?).to be false
+      end
+
+      it "does not support job submission" do
+        expect(owens.job_allow?).to be false
+      end
+
+      it "returns same value for multiple invocations" do
+        expect(owens.job_allow?).to be false
+        expect(owens.job_allow?).to be false
+        expect(owens.job_allow?).to be false
+        expect(owens.login_allow?).to be false
+        expect(owens.login_allow?).to be false
+        expect(owens.login_allow?).to be false
+        expect(owens.allow?).to be true
+        expect(owens.allow?).to be true
+        expect(owens.allow?).to be true
+      end
+
+      it "caches job_allow?" do
+        expect(owens).to receive(:allow?).once
+        2.times { owens.job_allow? }
+      end
+
+      it "caches login_allow?" do
+        expect(owens).to receive(:allow?).once
+        2.times { owens.login_allow? }
       end
     end
   end
