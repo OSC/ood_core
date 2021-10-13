41ebde04334a58c31a8bd5983f7edbda3db09912
Jeff Ohrstrom <johrstrom@osc.edu>
2021-10-13 14:51:24 -0400

    add some more tests

--- spec/cluster_spec.rb
+++ spec/cluster_spec.rb
@@ -57,12 +57,12 @@ describe OodCore::Cluster do
       end
 
       it 'can enable batch_connect_ssh_allow?' do
-        owens = OodCore::Cluster.new({id: "owens", batch_connect: { ssh_allow: true } })
+        owens = OodCore::Cluster.new({id: 'owens', batch_connect: { ssh_allow: true } })
         expect(owens.batch_connect_ssh_allow?).to be true
       end
 
       it 'can disable batch_connect_ssh_allow?' do
-        owens = OodCore::Cluster.new({id: "owens", batch_connect: { ssh_allow: false } })
+        owens = OodCore::Cluster.new({id: 'owens', batch_connect: { ssh_allow: false } })
         expect(owens.batch_connect_ssh_allow?).to be false
       end
 
@@ -103,30 +103,37 @@ describe OodCore::Cluster do
 
     it "responds to ccq?" do
       expect(clusters.select { |cluster| cluster.ccq? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'ccq' }}).ccq?).to eq(true)
     end
 
     it "responds to kubernetes?" do
       expect(clusters.select { |cluster| cluster.kubernetes? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'kubernetes' }}).kubernetes?).to eq(true)
     end
 
     it "responds to linux_host?" do
       expect(clusters.select { |cluster| cluster.linux_host? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'linux_host' }}).linux_host?).to eq(true)
     end
 
     it "responds to lsf?" do
       expect(clusters.select { |cluster| cluster.lsf? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'lsf' }}).lsf?).to eq(true)
     end
 
     it "responds to pbspro?" do
       expect(clusters.select { |cluster| cluster.pbspro? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'pbspro' }}).pbspro?).to eq(true)
     end
 
     it "responds to sge?" do
       expect(clusters.select { |cluster| cluster.sge? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'sge' }}).sge?).to eq(true)
     end
 
     it "responds to slurm?" do
       expect(clusters.select { |cluster| cluster.slurm? }.size).to eql(0)
+      expect(OodCore::Cluster.new({id: 'owens', job: { adapter: 'slurm' }}).slurm?).to eq(true)
     end
 
     it "responds to torque?" do
@@ -135,6 +142,23 @@ describe OodCore::Cluster do
 
     it "fails on undefined things" do
       expect { clusters.select { |cluster| cluster.vmware? }.size }.to raise_error(NoMethodError)
+    end
+
+
+    it "does not pick up extra files" do
+      expect { OodCore::Cluster.new({id: 'owens'}).helper? }.to raise_error(NoMethodError)
+      expect { OodCore::Cluster.new({id: 'owens'}).drmaa? }.to raise_error(NoMethodError)
+    end
+
+    it "does not fail when no adapter is specified" do
+      expect(OodCore::Cluster.new({id: 'owens'}).ccq?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).kubernetes?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).linux_host?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).lsf?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).pbspro?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).sge?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).slurm?).to eq(false)
+      expect(OodCore::Cluster.new({id: 'owens'}).torque?).to eq(false)
     end
   end
 end
