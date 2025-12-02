4ce9b748611cd16dca494149435a085b1eefb739
Jeff Ohrstrom <johrstrom@osc.edu>
2025-12-02 14:14:40 -0500

    add a test case for slurm#accounts

--- test/job/adapters/slurm_test.rb
+++ test/job/adapters/slurm_test.rb
@@ -4,7 +4,7 @@ class TestSlurm < Minitest::Test
   include TestHelper
 
   def slurm_instance(config = {})
-    OodCore::Job::Factory.build({ adapter: 'slurm' }.merge(config))
+    OodCore::Job::Factory.build({ adapter: 'slurm', id: 'owens' }.merge(config))
   end
 
   def slurm_instance_fixture(cluster = 'glen')
@@ -34,5 +34,17 @@ class TestSlurm < Minitest::Test
 
     slurm = adapter.instance_variable_get(:@slurm)
     assert_equal(slurm.id, 'glen')
+  end
+
+  def test_account_info
+    adapter = slurm_instance
+    stub_etc
+    Open3.stubs(:capture3).with(
+      {}, 'sacctmgr', '-nP', 'show', 'users', 'withassoc', 'format=account,qos', 'where', 'user=me', 'cluster=owens', stdin_data: ''
+    ).returns([File.read('spec/fixtures/output/slurm/sacctmgr_show_accts_owens.txt'), '', exit_success])
+
+    accounts = adapter.accounts
+    assert_equal(accounts.map(&:name), ["pzs1124", "pzs1118", "pzs1117", "pzs1010", "pzs0715", "pzs0714", "pde0006", "pas2051", "pas1871", "pas1754", "pas1604"])
+    accounts.each { |account| assert_equal(account.cluster, 'owens') }
   end
 end
