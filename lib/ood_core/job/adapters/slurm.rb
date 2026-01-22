c19e53fbadb121a071ec98e1369290a672149e4b
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2026-01-22 09:00:30 +0000

    Add to_i cast to gres_lines

--- lib/ood_core/job/adapters/slurm.rb
+++ lib/ood_core/job/adapters/slurm.rb
@@ -129,8 +129,8 @@ module OodCore
                             total_nodes: (node_cpu_info[3].to_i - node_cpu_info[2].to_i),
                             active_processors: node_cpu_info[5].to_i,
                             total_processors: (node_cpu_info[8].to_i - node_cpu_info[7].to_i),
-                            active_gpus: gres_lines.sum { |line| Slurm.gpus_from_gres(line[2]) },
-                            total_gpus: gres_lines.sum { |line| Slurm.gpus_from_gres(line[1]) }
+                            active_gpus: gres_lines.sum { |line| Slurm.gpus_from_gres(line[2]) }.to_i,
+                            total_gpus: gres_lines.sum { |line| Slurm.gpus_from_gres(line[1]) }.to_i
             )
           end
 
