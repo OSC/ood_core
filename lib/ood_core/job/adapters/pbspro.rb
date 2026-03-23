b5d291fe21933e47681cd51ce72ce9611d07d33f
Mike Robbert <mrobbert@ucar.edu>
2026-03-23 09:27:39 -0600

    Add cluster_info API for PBSPro job adapter

--- lib/ood_core/job/adapters/pbspro.rb
+++ lib/ood_core/job/adapters/pbspro.rb
@@ -1,4 +1,5 @@
 require "time"
+require "json"
 require "ood_core/refinements/hash_extensions"
 require "ood_core/job/adapters/helper"
 
@@ -83,6 +84,52 @@ module OodCore
             @strict_host_checking = strict_host_checking
             @pbs_exec             = pbs_exec && Pathname.new(pbs_exec.to_s)
             @bin_overrides        = bin_overrides
+          end
+
+	  def get_resource(resources, key)
+	    val = resources.fetch(key, 0)
+	    val.to_i
+	  end
+
+          def get_cluster_info
+            args = ["-a", "-F", "json"]
+            stdout = call("pbsnodes", *args)
+            node_info = JSON.parse(stdout)
+
+            nodes = node_info.fetch('nodes', {})
+
+            total_nodes = 0
+            allocated_nodes = 0
+            total_cpus = 0
+            allocated_cpus = 0
+            total_gpus = 0
+            allocated_gpus = 0 
+
+            nodes.each do |_node_name, node|
+              total_nodes += 1
+              resources_avail = node.fetch('resources_available', {})
+              total_cpus += get_resource(resources_avail, 'ncpus')
+	      total_gpus      += get_resource(resources_avail, 'ngpus')
+
+	      # Resources assigned (currently allocated to jobs)
+	      resources_assigned = node.fetch('resources_assigned', {})
+	      ncpus_assigned     = get_resource(resources_assigned, 'ncpus')
+	      ngpus_assigned     = get_resource(resources_assigned, 'ngpus')
+
+	      allocated_cpus += ncpus_assigned
+	      allocated_gpus += ngpus_assigned
+
+	      # A node is allocated if at least one CPU has been assigned to a job
+	      allocated_nodes += 1 if ncpus_assigned > 0
+	    end
+
+            ClusterInfo.new(active_nodes: allocated_nodes,
+                            total_nodes: total_nodes,
+                            active_processors: allocated_cpus,
+                            total_processors: total_cpus,
+                            active_gpus: allocated_gpus,
+                            total_gpus: total_gpus
+            )
           end
 
           # Get a list of hashes detailing each of the jobs on the batch server
@@ -297,6 +344,10 @@ module OodCore
           @pbspro.submit_string(script.content, args: args, chdir: script.workdir)
         rescue Batch::Error => e
           raise JobAdapterError, e.message
+        end
+
+        def cluster_info
+          @pbspro.get_cluster_info
         end
 
         # Retrieve info for all jobs from the resource manager
