327fa3edb4da5315b167d07c733dae9dea91dea9
bstepanovski <stepanovski.bojan@gmail.com>
2026-09-21 23:46:38 -0400

    Remove unnecessary blank line in kubernetes.rb

--- lib/ood_core/job/adapters/kubernetes.rb
+++ lib/ood_core/job/adapters/kubernetes.rb
@@ -140,7 +140,7 @@ module OodCore
         def supports_job_arrays?
           false
         end
-        
+
         # Retrieve job info from the resource manager
         # @abstract Subclass is expected to implement {#info}
         # @raise [NotImplementedError] if subclass did not define {#info}
