e963c7470bfd4fc52565d99c904944ee0af355c9
Travis Ravert <travert@osc.edu>
2026-09-23 19:11:45 -0400

    consolidates sacct fixtures and fixes memory_from_tres for decimals

--- test/job/adapters/slurm_test.rb
+++ test/job/adapters/slurm_test.rb
@@ -118,21 +118,17 @@ class TestSlurm < Minitest::Test
   end
 
   def test_info_historic_uses_alloc_tres
-    adapter = slurm_instance
-    fields = 'User,Group,JobId,JobName,Elapsed,ReqMem,AllocCPUS,ReqCPUS,Timelimit,' \
-             'State,TotalCPU,MaxRSS,Partition,Submit,Start,End,ReqTRES,AllocTRES'
-    Open3.stubs(:capture3).with(
-      {}, 'sacct', '-P', '--delimiter', "\u001F", '-n', '--units', 'G',
-      '--allocations', '-o', fields, stdin_data: ''
-    ).returns([File.read('spec/fixtures/output/slurm/sacct_alloc_tres.txt'), '', exit_success])
-  
-    jobs = adapter.info_historic
-    gpu_job = jobs.find { |job| job.id == '123456' }
-  
-    # AllocTRES says gres/gpu=2 while ReqTRES says gres/gpu=1
-    assert_equal(2, gpu_job.gpus)
-    # AllocTRES says mem=32G; previously info_historic set no memory at all
-    assert_equal(32 * 1024**3, gpu_job.total_memory)
+    batch = OodCore::Job::Adapters::Slurm::Batch.new(
+      conf: '/etc/slurm/conf/',
+      bin: nil,
+      bin_overrides: { 'sacct' => 'spec/fixtures/scripts/sacct.rb' }
+    )
+    jobs = OodCore::Job::Adapters::Slurm.new(slurm: batch).info_historic
+    job = jobs.find { |j| j.id == '20251' }
+
+    # ReqTRES says mem=0.98G but AllocTRES says mem=1.96G
+    assert_equal(2_104_533_975, job.total_memory)
+    assert_equal(1, job.gpus)
   end
 
   def test_gpus_from_tres_sums_multiple_types_without_rollup
